5138b4eddb3feb0843b38986cc8e47abb832b89b
K SRUTI RANI <111694006+sruti-rani@users.noreply.github.com>
2024-12-09 18:02:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  alert("Menu button clicked!");
+});
+document.querySelector(".login-btn").addEventListener("click", () => {
+  alert("Login button clicked!");
+});
